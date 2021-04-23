357430d184903b8649a7ba4d911a7f5601079137
Gabriel Alves Queiroz Pontes <gaqp@cin.ufpe.br>
2021-04-22 21:24:03 -0300

    Removido arquivos

--- cartridges/plugin_instorepickup/cartridge/client/default/js/checkout.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var processInclude = require('base/util');
-
-$(document).ready(function () {
-    processInclude(require('./checkout/checkout'));
-    processInclude(require('./checkout/instore'));
-});
