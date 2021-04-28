61cfa111ef47bccc9ff94d83702b06b792be0dc8
Gabriel Alves Queiroz Pontes <gaqp@cin.ufpe.br>
2021-04-28 20:51:08 -0300

    Page design

---
+++ cartridges/app_custom_gab/cartridge/controllers/Home.js
@@ -0,0 +1,30 @@
+'use strict'
+var server = require('server');
+var base = require('app_storefront_base/cartridge/controllers/Home');
+server.extend(base);
+
+var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+var cache = require('*/cartridge/scripts/middleware/cache');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+
+server.replace('Show', consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
+    var Site = require('dw/system/Site');
+    var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
+    var PageMgr = require('dw/experience/PageMgr');
+    var page = PageMgr.getPage('homepage-example');
+
+    if (page != null && page.isVisible()) {
+        if (!page.hasVisibilityRules()) {
+            res.cachePeriod = 168; // eslint-disable-line no-param-reassign
+            res.cachePeriodUnit = 'hours'; // eslint-disable-line no-param-reassign
+        }
+
+        res.page(page.ID, {});
+    } else {
+        pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
+        res.render('/home/homePage');
+    }
+    next();
+}, pageMetaData.computedPageMetaData);
+
+module.exports = server.exports();
