ebba42e7255a3c1d9c92920743ae98bb11587a4b
Gabriel Alves Queiroz Pontes <gaqp@cin.ufpe.br>
2021-04-28 16:29:26 -0300

    email de suporte

--- cartridges/app_custom_vlr/cartridge/controllers/scripts/hooks/sendMailAfterContact.js
+++ cartridges/app_custom_vlr/cartridge/controllers/scripts/hooks/sendMailAfterContact.js
@@ -1,20 +1,24 @@
 var emailHelper = require('*/cartridge/scripts/helpers/emailHelpers');
 var Resource = require('dw/web/Resource');
-
+var customObjectMgr = require('dw/object/CustomObjectMgr');
 // eslint-disable-next-line require-jsdoc
 var sendMailAfterContact = function (firstName, lastName, contactEmail, contactTopic, contactComment) {
-    var emailObj = {
-        to: contactEmail,
-        subject: Resource.msg('custom.email.contact.title', 'customEmail', null),
-        from: contactEmail
-    };
-    var data = {
-        firstName: firstName,
-        lastName: lastName,
-        contactEmail: contactEmail,
-        contactTopic: contactTopic,
-        contactComment: contactComment
-    };
-    emailHelper.sendEmail(emailObj, 'customEmail/contactEmail', data);
+    var mailList = customObjectMgr.getAllCustomObjects('ID').asList().toArray();
+    mailList.map((mail) => {
+        var emailObj = {
+            to: mail.custom.Email,
+            subject: Resource.msg('custom.email.contact.title', 'customEmail', null),
+            from: contactEmail
+        };
+        var data = {
+            firstName: firstName,
+            lastName: lastName,
+            contactEmail: contactEmail,
+            contactTopic: contactTopic,
+            contactComment: contactComment
+        };
+        emailHelper.sendEmail(emailObj, 'customEmail/contactEmail', data);
+
+    });
 };
 exports.sendMailAfterContact = sendMailAfterContact;
