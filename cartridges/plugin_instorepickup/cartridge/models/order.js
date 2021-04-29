0c9e17a6c0ccf22f604df764eee55e9f5f886a60
Gabriel Alves Queiroz Pontes <gaqp@cin.ufpe.br>
2021-04-29 13:41:48 -0300

    Order status

--- cartridges/plugin_instorepickup/cartridge/models/order.js
+++ cartridges/plugin_instorepickup/cartridge/models/order.js
@@ -18,6 +18,8 @@ function OrderModel(lineItemContainer, options) {
         this.firstLineItem.isPickUpInStore = lineItemContainer.productLineItems[0] ?
             lineItemContainer.productLineItems[0].shipment.shippingMethod.custom.storePickupEnabled : null;
     }
+    this.shippingStatus = lineItemContainer.shippingStatus ?
+    lineItemContainer.shippingStatus : null;
 }
 
 OrderModel.prototype = Object.create(base.prototype);
